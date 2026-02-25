acaf022722370e1993f0f6053128c6fdaa19bf42
Bruno Sena <BrunoSena14@outlook.com>
2026-02-25 19:12:19 -0300

    initial project

---
+++ src/main/java/br/com/corecode/msecowatt/MsecowattApplication.java
@@ -0,0 +1,13 @@
+package br.com.corecode.msecowatt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsecowattApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsecowattApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/corecode/msecowatt/MsecowattApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.corecode.msecowatt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsecowattApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
